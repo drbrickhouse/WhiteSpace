b09be1d12dbc2a0c86f39d0d19d20e47f1ce0625
Dillon Brickhouse <trombonecricket@gmail.com>
2018-01-06 16:40:35 -0700

    Re-enabled WooCommerce default CSS

--- functions.php
+++ functions.php
@@ -63,8 +63,6 @@ require_once('wp_bootstrap_navwalker.php');
 function woocommerce_setup() {
   //Declare WooCommerce Support
   add_theme_support( 'woocommerce' );
-  //Disable WooCommerce CSS
-  add_filter( 'woocommerce_enqueue_styles', '__return_false' );
 }
 
 add_action( 'after_setup_theme', 'woocommerce_setup' );
