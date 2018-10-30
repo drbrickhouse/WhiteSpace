fff341a0dca5a16186d76b2805d50bb7657917fc
Dillon Brickhouse <trombonecricket@gmail.com>
2018-10-30 15:33:49 -0600

    Bug fixes

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
                               <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?> | <a href="/sitemap/" title="sitemap">Sitemap</a></p>
                             </div>
                             <div class="col-xs-12 col-md-6">
-                              <p>Site by: <a href="https://tenpeaksdigital.com/" target="_blank"><img src="<?php echo get_template_directory_uri() . "/images/ten-peaks-logo.png" ?>"></a>
+                              <p>Site by: <img src="<?php echo get_template_directory_uri() . "/images/site-logo.png" ?>">
                             </div>
                           </div>
                         </div>
